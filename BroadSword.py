d4d749bc0beabc2e5cb3039e47f1b0bc25fd5aec
Cody-Somers <112672306+Cody-Somers@users.noreply.github.com>
2023-05-24 22:54:00 +0000

    Added findBands function successfully. Using data that has been put in broadsword before

--- BroadSword.py
+++ BroadSword.py
@@ -14,7 +14,7 @@ Lorentz = np.zeros([3500,3500]) # Lorentz broadening matrix for each spectrum
 Disorder = np.zeros([3500,3500]) # Disorder broadening matrix for each spectrum
 
 ExpSXSCount = np.zeros([2],dtype=int) # Stores number of elements in the arrays
-#CalcSXSCase = np.zeros(0,dtype=int) # The number of sites used for the project
+CalcSXSCase = 0
 CalcSXSCount = np.zeros([3,40],dtype=int) # Stores number of elements in the arrays
 BroadSXSCount = np.zeros([3,40],dtype=int) # Stores number of elements in the arrays
 SumSXSCount = np.zeros([3],dtype=int)
@@ -29,30 +29,17 @@ shiftXES = np.zeros([40,50])
 scalar = np.zeros([3,40])
 Edge = np.zeros([40],dtype=str)
 Site = np.zeros([40])
-SiteCount = 0
 
 # Misc
 bandshift = np.zeros([40,40])
 bands_temp = np.zeros([3500,40,40])
 bands_temp_count = np.zeros([40,40],dtype=int)
 
-# print("Hello")
-# print(ExpSXSCount)
-# # print(CalcSXSCase)
-# CalcSXSCase = 5
-# print(CalcSXSCase)
-
-# So the plan is to take in the calculations as a text file which we have to specify the name somehow...
-# OR we could use the XAS loader from patrick that would allow us to be able to just open it from jupyter.
-# I think having hardcoded text files would be easier though.
-
-# THen for the txspec we will just load it the same way, so its two birds with one stone. Have a function that takes in all of the files
-# Then it will deal with them appropriately. We need multiple functions though for multiple files of that type.
-
-# We
-
 class Broaden():
-    """Class to take in data and broaden it."""
+    """Class to take in data and broaden it.
+    We have to load the experimental, and then load the calculations. With the calculations we can load multiple different sets of files
+    so that we account for the different sites that can exist. Just call the loadCalc function multiple times and it should account for the diffent sites.
+    """
 
     def __init__(self):
         self.data = list()
@@ -64,20 +51,20 @@ class Broaden():
         basedir : string
             Specifiy the absolute or relative path to experimental data.
         XES, XANES : string
-            Specify the file name (either ASCII or HDF5).
+            Specify the file name (ASCII).
         """
 
         with open(basedir+"/"+XES, "r") as xesFile:
-            df = pd.read_csv(xesFile, delimiter=' ',header=None)
+            df = pd.read_csv(xesFile, delimiter='\s+',header=None) # Change to '\s*' and specify engine='python' if this breaks in jupyter notebook
             c1 = 0
-            for i in range(len(df)):
+            for i in range(len(df)): 
                 ExpSXS[0][c1][0] = df[0][c1] # Energy
                 ExpSXS[1][c1][0] = df[1][c1] # Counts
                 c1 += 1
             ExpSXSCount[0] = c1 # Length
 
         with open(basedir+"/"+XANES, "r") as xanesFile:
-            df = pd.read_csv(xanesFile, delimiter=' ',header=None)
+            df = pd.read_csv(xanesFile, delimiter='\s+',header=None)
             c1 = 0
             for i in range(len(df)):
                 ExpSXS[0][c1][1] = df[0][c1] # Energy
@@ -93,37 +80,61 @@ class Broaden():
         basedir : string
             Specifiy the absolute or relative path to experimental data.
         XES, XAS, XANES : string
-            Specify the file name (either ASCII or HDF5).
+            Specify the file name (.txspec).
         """
-        global SiteCount
+        global CalcSXSCase
 
         with open(basedir+"/"+XES, "r") as xesFile:
-            df = pd.read_csv(xesFile, delimiter=' ',header=None)
+            df = pd.read_csv(xesFile, delimiter='\s+',header=None)
             c1 = 0
             for i in range(len(df)):
-                CalcSXS[0][c1][0][SiteCount] = df[0][c1] # Energy
-                CalcSXS[1][c1][0][SiteCount] = df[1][c1] # Counts
+                CalcSXS[0][c1][0][CalcSXSCase] = df[0][c1] # Energy
+                CalcSXS[1][c1][0][CalcSXSCase] = df[1][c1] # Counts
                 c1 += 1
-            CalcSXSCount[0] = c1
+            CalcSXSCount[0][CalcSXSCase] = c1 # Length for each Site
 
         with open(basedir+"/"+XAS, "r") as xasFile:
-            df = pd.read_csv(xasFile, delimiter=' ',header=None)
+            df = pd.read_csv(xasFile, delimiter='\s+',header=None)
             c1 = 0
             for i in range(len(df)):
-                CalcSXS[0][c1][1][SiteCount] = df[0][c1] # Energy
-                CalcSXS[1][c1][1][SiteCount] = df[1][c1] # Counts
+                CalcSXS[0][c1][1][CalcSXSCase] = df[0][c1] # Energy
+                CalcSXS[1][c1][1][CalcSXSCase] = df[1][c1] # Counts
                 c1 += 1
-            CalcSXSCount[1] = c1
+            CalcSXSCount[1][CalcSXSCase] = c1 # Length for each Site
 
         with open(basedir+"/"+XANES, "r") as xanesFile:
-            df = pd.read_csv(xanesFile, delimiter=' ',header=None)
+            df = pd.read_csv(xanesFile, delimiter='\s+',header=None)
             c1 = 0
             for i in range(len(df)):
-                CalcSXS[0][c1][2][SiteCount] = df[0][c1] # Energy
-                CalcSXS[1][c1][2][SiteCount] = df[1][c1] # Counts
+                CalcSXS[0][c1][2][CalcSXSCase] = df[0][c1] # Energy
+                CalcSXS[1][c1][2][CalcSXSCase] = df[1][c1] # Counts
                 c1 += 1
-            CalcSXSCount[2] = c1
-            
-        SiteCount += 1
+            CalcSXSCount[2][CalcSXSCase] = c1 # Length for each Site
+
+        CalcSXSCase += 1
         return
 
+    def FindBands(self):
+        c1 = 0
+        while c1 < CalcSXSCase: # For each site (.loadCalc)
+            starter = False
+            c3 = 0
+            c2 = 0
+            while c2 < CalcSXSCount[0][c1]: # For each data point
+                if starter is False:
+                    if CalcSXS[1][c2][0][c1] != 0: # Spectrum is not zero
+                        Bands[c3][c1][0] = CalcSXS[0][c2][0][c1] # Start point of band
+                        starter = True
+                if starter is True:
+                    if CalcSXS[1][c2][0][c1] == 0: # Spectrum hits zero
+                        Bands[c3][c1][1] = CalcSXS[0][c2][0][c1] # End point of band
+                        starter = False
+                        c3 += 1
+                c2 += 1
+            BandNum[c1] = c3 # The number of bands in each spectrum
+            c1 += 1
+        return
+    
+    def Shift(self):
+
+        return
--- loadtest.py
+++ loadtest.py
@@ -1,5 +1,7 @@
 from BroadSword import *
 
 broad = Broaden()
-broad.loadExp(".","xes.txt","xanes.txt")
+broad.loadExp(".","expxes.txt","expxanes.txt")
+broad.loadCalc(".","actualxes.txt","actualxas.txt","actualxanes.txt")
 broad.loadCalc(".","xes.txt","xas.txt","xanes.txt")
+broad.FindBands()
